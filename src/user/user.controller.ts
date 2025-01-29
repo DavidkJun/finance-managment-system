6c258a6c010c7b302934e4783a44d3e5faf596fc
Davyd Krykota <krikotadavid957@gmail.com>
2025-01-29 11:42:41 +0200

    added get and post

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+  @Get(':id')
+  findAll(@Param("id") id:string) {
+    return {user :{
+      id:id,
+      }
+    }
+  }
+  @Post()
+  create() {
+    return "This route is for creating user"
+  }
+}
